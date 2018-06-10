123c6bf88cdc198f195d3e658dbb8ab587c4f6d3
Lightyaer <mmnyed@gmail.com>
2018-06-10 13:12:28 +0530

    Server modified to ES6

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-require('./config/config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { mongoose } = require('../db/mongoose');
-var driverRoutes = require('./routes/user/driver');
-var retailerRoutes = require('./routes/user/retailer');
-var salesOrderRoutes = require('./routes/user/salesOrder');
-var adminRoutes = require('./routes/admin/admin');
+import "./config/config";
+import express from "express";
+import { json } from "body-parser";
+import { mongoose } from "../db/mongoose";
+import driverRoutes from "./routes/user/driver";
+import retailerRoutes from "./routes/user/retailer";
+import salesOrderRoutes from "./routes/user/salesOrder";
+import adminRoutes from "./routes/admin/admin";
 
 
 
 var app = express();
 var PORT = process.env.PORT;
 
-app.use(bodyParser.json());
+app.use(json());
 
 app.use('/admin', adminRoutes);
 
@@ -26,6 +26,6 @@ app.listen(PORT, () => {
     console.log(`Server is up on Port ${PORT}`);
 })
 
-module.exports = {
+export default {
     app
-}
+};
